4c995867fb85f322bac317f90297b3fe35c64ed0
Karun Raj Pitta <129605215+karunraj07@users.noreply.github.com>
2024-11-12 22:54:19 +0530

    Update and rename vite.config.ts to vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+//import { defineConfig } from 'vite';
+//import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+//  server: {
+//        host: '0.0.0.0',
+//        port: 5173
+//    }
+  //plugins: [react()],
+//});
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173
+    }
+});
