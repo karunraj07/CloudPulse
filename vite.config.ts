1748abf52c1a5b71390a617f3dc7fd03b0d85014
Karun Raj Pitta <129605215+karunraj07@users.noreply.github.com>
2024-11-12 22:59:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
-//import { defineConfig } from 'vite';
-//import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-//export default defineConfig({
-//  server: {
-//        host: '0.0.0.0',
-//        port: 5173
-//    }
-  //plugins: [react()],
-//});
-import { defineConfig } from 'vite';
-
 export default defineConfig({
-    server: {
+  server: {
         host: '0.0.0.0',
         port: 5173
-    }
+    },
+  plugins: [react()],
 });
